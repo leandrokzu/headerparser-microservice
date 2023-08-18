7ec48d25dddde9274dadc97343fc16b506a9535c
Leandro Augusto <leandroaugusto470@gmail.com>
2023-08-18 17:53:44 -0300

    ajustando o maneira para pegar os headers

--- index.js
+++ index.js
@@ -21,14 +21,10 @@ app.get('/', function (req, res) {
 
 // your first API endpoint...
 app.get('/api/whoami', function (req, res) {
-  const ipAddress = req.ip;
-  const userAgent = req.get('User-Agent');
-  const preferredLanguage = req.get('Accept-Language');
-
   res.json({
-    ipaddress: ipAddress,
-    software: userAgent,
-    language: preferredLanguage,
+    ipaddress: req.ip,
+    language: req.get('Accept-language'),
+    software: req.get('User-Agent'),
   });
 });
 
